cb5c239a995b3b8d0a8bbe21b8977686d6add3a3
Alex Cruz <du.alexcruz@gmail.com>
2017-09-01 03:11:09 +0200

    Push pixel theme overlay This quickly allows us to create more themes based on the pixel theme found inside the Oreo factory image.
    Thanks to Viperusgit for finding this and letting us know!

--- products/themes.mk
+++ products/themes.mk
@@ -4,31 +4,35 @@
 # Deep Purple
 PRODUCT_PACKAGES += \
     DeepPurpleTheme
-	
+
 # Indigo
 PRODUCT_PACKAGES += \
     IndigoTheme
-	
+
 # Pink
 PRODUCT_PACKAGES += \
     PinkTheme
-	
+
+# Pixel
+PRODUCT_PACKAGES += \
+    PixelTheme
+
 # Purple
 PRODUCT_PACKAGES += \
     PurpleTheme
-	
+
 # Red
 PRODUCT_PACKAGES += \
     RedTheme
-	
+
 # Sky Blue
 PRODUCT_PACKAGES += \
     SkyBlueTheme
-	
+
 # Yellow
 PRODUCT_PACKAGES += \
     YellowTheme
-	
+
 # Teal
 PRODUCT_PACKAGES += \
     TealTheme
--- themes/Pixel/Android.mk
+++ themes/Pixel/Android.mk
@@ -1,8 +1,20 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := PixelTheme
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
 include $(BUILD_PACKAGE)
+=======
+
+LOCAL_PACKAGE_NAME := PixelTheme
+LOCAL_RRO_THEME := PixelTheme
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+include $(BUILD_RRO_PACKAGE)
+>>>>>>> 375792b... Push pixel theme overlay
