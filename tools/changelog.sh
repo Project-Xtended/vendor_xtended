a6f363c6d74e3bd3a80dec015749af07de08bc66
dadi11 <dadi.sikorski@gmail.com>
2017-11-04 14:19:34 +0100

    vendor: update changelog script
    * improved the look and readability
    * use %F (2017-12-30) instead %m-%d-%Y (12-30-2017)
    * show only 7 first chars of commit sha
    * remove / at the end of repo names
    * seperate each repo in changelog

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -11,23 +11,19 @@ touch $Changelog
 
 echo "Generating changelog..."
 
-for i in $(seq 10);
+for i in $(seq 7);
 do
-export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+export After_Date=`date --date="$i days ago" +%F`
 k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
-
-	# Line with after --- until was too long for a small ListView
-	echo '=======================' >> $Changelog;
-	echo  "     "$Until_Date       >> $Changelog;
-	echo '=======================' >> $Changelog;
-	echo >> $Changelog;
-
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
-	echo >> $Changelog;
+export Until_Date=`date --date="$k days ago" +%F`
+echo "====================" >> $Changelog;
+echo "     $Until_Date    " >> $Changelog;
+echo "====================" >> $Changelog;
+repo forall -pc 'git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7' >> $Changelog
+echo "" >> $Changelog;
 done
 
-sed -i 's/project/   */g' $Changelog
+sed -i 's/project/ */g' $Changelog
+sed -i 's/[/]$//' $Changelog
 
 cp $Changelog $OUT/
