e7036bcd6155583c2f85531966df5f508681f4ed
ElDainosor <eldainosor@gmail.com>
2017-12-23 03:55:35 +0000

    [2/2] Add 'GoBold' font family
    Required for lockscreen. Also, added in case of adding more fonts.
    
    based on this commit, credits to beanstown and jhenrique
    https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb

--- config/common.mk
+++ config/common.mk
@@ -122,6 +122,14 @@ PRODUCT_PACKAGES += \
     ntfsfix \
     ntfs-3g
 
+# Xtended Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/xtended/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/xtended/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/xtended/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/xtended/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/xtended/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/xtended/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
 
 PRODUCT_PACKAGES += \
     charger_res_images
