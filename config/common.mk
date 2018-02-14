nitrogen-project/android_vendor_nitrogen

add support for building custom themes

products/common.mk
@@ -91,3 +91,6 @@ PRODUCT_PACKAGES += \
 
 # Boot animations
 $(call inherit-product-if-exists, vendor/nitrogen/products/bootanimation.mk)
+
+# Themes
+$(call inherit-product-if-exists, vendor/nitrogen/products/themes.mk)

Project-Xtended/vendor_xtended

config/common.mk
@@ -182,6 +182,9 @@ PRODUCT_COPY_FILES += \
 endif
 endif
 
+# Themes
+$(call inherit-product-if-exists, vendor/xtended/products/themes.mk)
+
 # Versioning System
 # Xtended first version.
 PRODUCT_VERSION_MAJOR = 8.1