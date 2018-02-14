4aa8530a4f3714b4c56836d4290e6eb99d686da8
xyyx <xyyx@mail.ru>
2018-02-15 00:08:14 +0800

    themes: build as apps

--- themes/Purple/Android.mk
+++ themes/Purple/Android.mk
@@ -1,11 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_PACKAGE_NAME := PurpleTheme
-LOCAL_RRO_THEME := PurpleTheme
-LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-include $(BUILD_RRO_PACKAGE)
+LOCAL_PACKAGE_NAME := PurpleTheme
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)
