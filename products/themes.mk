78596a20d869af7a9563fbc999b0be4584176a4c
Alex Cruz <du.alexcruz@gmail.com>
2018-02-15 19:09:03 -0500

    Fruity pebbles [2/3]
    *Dark theme edits*
    - Add in styles to theme the package installer
    - Theme DND QS panel text
    - Theme conditions/suggestions background
    - Delete our old overlays
    
    Change-Id: Ie6e916691449af4b4c3d652d2be6fd9924ac3c8b
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- products/themes.mk
+++ products/themes.mk
@@ -1,38 +1,27 @@
 # Theme vendor extension
--include vendor/nitrogen/themes/Android.mk
+-include vendor/xtended/themes/Android.mk
 
-# Deep Purple
 PRODUCT_PACKAGES += \
-    DeepPurpleTheme
+    PixelTheme \
+    AmberAccent \
+    BlackAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    TealAccent \
+    YellowAccent \
+    WhiteAccent
 
-# Indigo
-PRODUCT_PACKAGES += \
-    IndigoTheme
-
-# Pink
-PRODUCT_PACKAGES += \
-    PinkTheme
-
-# Pixel
-PRODUCT_PACKAGES += \
-    PixelTheme
-
-# Purple
-PRODUCT_PACKAGES += \
-    PurpleTheme
-
-# Red
-PRODUCT_PACKAGES += \
-    RedTheme
-
-# Sky Blue
-PRODUCT_PACKAGES += \
-    SkyBlueTheme
-
-# Yellow
-PRODUCT_PACKAGES += \
-    YellowTheme
-
-# Teal
-PRODUCT_PACKAGES += \
-    TealTheme
