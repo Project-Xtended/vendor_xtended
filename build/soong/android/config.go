0b1c659989357fd8b1cf046e3917c17b86ea6693
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 14:23:57 -0700

    vendor/xtended: Add support for java source overlays
    *) Set JAVA_SOURCE_OVERLAYS in a device makefile as follows:
       # Java sourcefile overlays (for Android.bp built modules only)
       # Format is a whitespace separated set of rules, each of which
       # structured as follows:
       # modulename|overlay directory|glob pattern within overlay dir
       JAVA_SOURCE_OVERLAYS := \
               org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
    
    *) As of this commit, java sources overlays are only allowed for
       module org.lineageos.hardware.
    
    Change-Id: I6be1c12567081357f5231a84df98ac002c0563b4

---
+++ build/soong/android/config.go
@@ -0,0 +1,12 @@
+package android
+
+// Global config used by Lineage soong additions
+var XtendedConfig = struct {
+	// List of packages that are permitted
+	// for java source overlays.
+	JavaSourceOverlayModuleWhitelist []string
+}{
+	// JavaSourceOverlayModuleWhitelist
+	[]string{
+	},
+}
--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -62,6 +62,7 @@ type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
+	Java_Source_Overlays *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
