ef7e707a117fe7c931732fb02d57576c62d37a7a
Christian Oder <myself5@carbonrom.org>
2018-08-30 23:09:46 +0200

    vendor: Add turbo privapp-permissions whitelist
    Change-Id: I3e36da13add944bedd1a186ffb167e20119e6992

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -18,3 +18,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/xtended_arm.mk \
     $(LOCAL_DIR)/xtended_arm64.mk \
     $(LOCAL_DIR)/xtended_x86.mk
+
+########################
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-turbo.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
