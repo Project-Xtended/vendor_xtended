d0627c1d1814fcddc4ba8f89279908f0512b1bad
Bauuuuu <abansode@outlook.com>
2018-09-24 20:45:14 +0200

    vendor: device official check for updater
    Change-Id: I50aed1f0082bacddf129feae83770eb4bc15811a

--- config/version.mk
+++ config/version.mk
@@ -8,6 +8,21 @@ ifndef XTENDED_BUILD_TYPE
     XTENDED_BUILD_TYPE := XPLOITED
 endif
 
+ifeq ($(XTENDED_BUILD_TYPE), OFFICIAL)
+   LIST = $(shell cat vendor/xtended/xtended.devices)
+    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+
+PRODUCT_PACKAGES += \
+    Updater
+
+    endif
+    ifneq ($(IS_OFFICIAL), true)
+       XTENDED_BUILD_TYPE := XPLOITED
+       $(error Device is not official "$(CURRENT_DEVICE)")
+    endif
+endif
+
 TARGET_PRODUCT_SHORT := $(subst xtended_,,$(XTENDED_BUILD))
 
 XTENDED_VERSION := Xtended-$(XTENDED_MAJOR_VERSION)-$(XTENDED_RELEASE_VERSION)-$(XTENDED_BUILD)-$(XTENDED_BUILD_TYPE)-$(XTENDED_BUILD_DATE)
