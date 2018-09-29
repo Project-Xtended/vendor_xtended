5ee6a170b5f5588e15435c0f2d67cdf01be1a5f5
maniac103 <dannybaumann@web.de>
2018-09-29 17:18:10 +0530

    soong_config: Allow process-specific override of target SDK version

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -24,6 +24,9 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -35,4 +38,5 @@ type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Target_process_sdk_version_override *string `json:",omitempty"`
 }
