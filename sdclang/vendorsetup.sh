206ef8d5e610f1adc3e0bd808c8786fa1ae7660e
Miguel Martinez <slverwolf@gmail.com>
2018-10-20 17:41:15 -0500

    sdclang: fix common flags and add HD flags
    arm-implicit-it is incompatible with dragontc
    Enable polly
    Enable fused fp operations

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -4,7 +4,9 @@ export SDCLANG_PATH=vendor/qcom/sdclang/dragontc-7.0/bin
 export SDCLANG_LTO_DEFS=vendor/xtended/sdclang/sdllvm-lto-defs.mk
 export SDCLANG_COMMON_FLAGS="-Ofast -g0 -mcpu=cortex-a57 -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
--mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable"
+-Wno-inline-asm -Wno-unused-command-line-argument -Wno-unused-variable \
+-mllvm -polly -mllvm -polly-process-unprofitable -ffp-contract=fast \
+-mllvm -polly-optimized-scops -mllvm -polly-tiling=false"
 
 # Enable based on host OS/availablitiy
 case $(uname -s) in
