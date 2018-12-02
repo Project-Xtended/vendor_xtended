9b31f54ef28dff37fe56ac44bcef8cd51b07c7a7
spezi77 <spezi7713@gmx.net>
2018-12-02 14:16:40 -0700

    Add 23 fancy accent colors
    Change-Id: I0112d7ca3e919302b80c7b13b28d9976e53e74f3

--- config/common.mk
+++ config/common.mk
@@ -223,7 +223,33 @@ PRODUCT_PACKAGES += \
     AccentDeadRed \
     AccentRottenOrange \
     AccentDeepPurple \
-    AccentBlackMagic
+    AccentBlackMagic \
+    AccentCandyRed \
+    AccentJadeGreen \
+    AccentPaleBlue \
+    AccentPaleRed \
+    AccentObfusBleu \
+    AccentNotImpPurple \
+    AccentHolillusion \
+    AccentMoveMint \
+    AccentFootprintPurple \
+    AccentBubblegumPink \
+    AccentFrenchBleu \
+    AccentManiaAmber \
+    AccentSeasideMint \
+    AccentDreamyPurple \
+    AccentSpookedPurple \
+    AccentHeirloomBleu \
+    AccentTruFilPink \
+    AccentWarmthOrange \
+    AccentColdBleu \
+    AccentDiffDayGreen \
+    AccentDuskPurple \
+    AccentBurningRed \
+    AccentHazedPink \
+    AccentColdYellow \
+    AccentNewHouseOrange \
+    AccentIllusionsPurple
 
 PRODUCT_PACKAGES += \
     PrimaryAlmostBlack \
