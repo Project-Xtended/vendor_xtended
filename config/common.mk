5f5aa942349c2944ccc8e63927cf8dfc2200c300
Lucchetto <lucchetto.tie@live.com>
2019-03-01 20:39:54 +0530

    Add Gradient Accent From RevengeOS

--- config/common.mk
+++ config/common.mk
@@ -266,7 +266,14 @@ PRODUCT_PACKAGES += \
     AccentHazedPink \
     AccentColdYellow \
     AccentNewHouseOrange \
-    AccentIllusionsPurple
+    AccentIllusionsPurple \
+    AccentFlare \
+    AccentSublime \
+    AccentSincityRed \
+    AccentOrangeCoral \
+    AccentPureLust \
+    AccentQuepal \
+    AccentShadesOfGrey
 
 PRODUCT_PACKAGES += \
     PrimaryAlmostBlack \
