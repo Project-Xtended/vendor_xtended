b0b630779630af8672dff9508c52aca7ffa1bada
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-16 14:01:19 -0700

    lineage: Rework DTBO image generation
    Currently we handle the DTBO images from start to finish, including
    AVB footers, and just drop them in $(PRODUCT_OUT)/dtbo.img when done.
    While this works for the most part, we are limited in what we can do
    with the DTBO images. For example non-A/B device recovery images must
    have a copy of the DTBO image appended to recovery (see AOSP docs:
    https://source.android.com/devices/bootloader/recovery-image ). This
    means that we need to pass the pre-AVB dtbo image into
    BOARD_PREBUILT_DTBOIMAGE in order to let the AOSP build system handle it.
    
    This commit reworks the way the DTBO images are built, placing them at
    $(PRODUCT_OUT)/dtbo-pre.img instead and setting BOARD_PREBUILT_DTBOIMAGE
    to that path so the AOSP build system can pick the images up and sign
    them and append to recovery (for applicable devices).
    
    Change-Id: I8819d7cd92ba298de67dbc7f6b321917ba8524f1

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -113,3 +113,9 @@ ifeq ($(HOST_OS),darwin)
   KERNEL_MAKE_FLAGS += LIBRARY_PATH=/usr/local/opt/openssl/lib
 endif
 
+# Set DTBO image locations so the build system knows to build them
+ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)
+BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo/arch/$(KERNEL_ARCH)/boot/dtbo.img
+else ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
+BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo-pre.img
+endif
