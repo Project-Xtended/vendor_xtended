f7096dedd1383c7bf0258af87125fc346b5bdae9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 03:16:18 -0300

    vendor: Add soong variable for TARGET_CAMERA_BOOTTIME_TIMESTAMP
    Change-Id: Ief38c409812ff0819a4906c3e1c5037532f47f18

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -77,6 +77,10 @@ type Product_variables struct {
 		Header_libs []string
 		Shared_libs []string
 	}
+	Needs_camera_boottime_timestamp struct {
+		Cflags []string
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
@@ -103,4 +107,5 @@ type ProductVariables struct {
 	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
+	Needs_camera_boottime_timestamp  *bool `json:",omitempty"`
 }
