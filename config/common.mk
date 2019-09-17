d294675c9629bc427ab18cea59484b60832cc66f
harsh-23 <harsh.v23labs@gmail.com>
2019-09-17 12:07:02 +0200

    accents: Add new accent colors for Q

--- config/common.mk
+++ config/common.mk
@@ -182,6 +182,20 @@ PRODUCT_PACKAGES += \
     HideCutout \
     StatusBarStock
 
+# Accents
+PRODUCT_PACKAGES += \
+    AccentColorYellowOverlay \
+    AccentColorVioletOverlay \
+    AccentColorTealOverlay \
+    AccentColorRedOverlay \
+    AccentColorQGreenOverlay \
+    AccentColorPinkOverlay \
+    AccentColorLightPurpleOverlay \
+    AccentColorIndigoOverlay \
+    AccentColorFlatPinkOverlay \
+    AccentColorCyanOverlay \
+    AccentColorBlueGrayOverlay
+
 # QS tile styles
 #PRODUCT_PACKAGES += \
     QStileCircleTrim \
