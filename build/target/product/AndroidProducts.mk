736c3f7d6a469498d79cc78aca28f19292c7b7e1
SuperDroidBond <superdroidbond@yahoo.com>
2019-09-18 01:08:20 +0530

    Remove Turbo Permissions *we're not force permission whitelist

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -19,11 +19,3 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/xtended_arm64.mk \
     $(LOCAL_DIR)/xtended_x86.mk \
     $(LOCAL_DIR)/xtended_x86_64.mk
-
-########################
-include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-turbo.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
