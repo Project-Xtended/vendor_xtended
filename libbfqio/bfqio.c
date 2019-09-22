4e5d563c26186b0a292fcf3d2a8d55f306cbd1d4
dianlujitao <dianlujitao@lineageos.org>
2019-09-22 14:07:43 +0530

    vendor: Add support for BFQIO cgroups
     * Introduce a standalone libbfqio to be picked by inputflinger,
       surfaceflinger and display HAL, without extending the VNDK.
    
     * This commit is based on the following patches:
    
      From: Steve Kondik <steve@cyngn.com>
      Date: Tue, 08 Sep 2015 02:23:41 -0700
    
          cutils: Add support for BFQIO cgroups
    
           * Add support for a custom hierarchy of cgroups on top of the BFQ
             IO scheduler. This allows us to place every thread into the
             right class (realtime/best-effort/idle) with a set up priority
             buckets depending on use case.
           * The benefit of doing this is pretty incredible from an
             interactivity standpoint. Realtime users (display/audio) benefit
             the most, resulting in glitch-free audio and jank-free video.
             Dexopting in the background no longer causes active harm to
             foreground tasks. Other tasks such as account syncing become
             invisible from the user's perspective.
           * Magic bullet? Perhaps.
    
          Change-Id: I4eb911395364ce46d6dcbff43e94286ded03a97d
    
      From: Steve Kondik <steve@cyngn.com>
      Date: Sat, 19 Sep 2015 12:43:40 -0700
    
          cutils: Fix copypasta in ioprio code
    
          Change-Id: I976693be4a6913ed2090fa32d0f8e4c93657e7e7
    
      From: Steve Kondik <steve@cyngn.com>
      Date: Sun, 20 Sep 2015 01:08:07 -0700
    
          cutils: Fix display ioprio
    
           * A few unwanted items are sneaking into this class resulting in
             system slowdowns. Fixit.
    
          Change-Id: I29033a38adf2c65535f916135d395d5147afe323
    
      From: Steve Kondik <steve@cyngn.com>
      Date: Sun, 20 Sep 2015 03:00:54 -0700
    
          cutils: Clean up ioprio grouping
    
           * It's now better understood what's happening system-wide due to these
             changes, so clean up the stuff which is not necessary and/or
             potentially harmful.
    
          Change-Id: I72178770844c1ac388a0b424b12cdc49042aec87
    
      From: Steve Kondik <steve@cyngn.com>
      Date: Sun, 20 Sep 2015 17:36:54 -0700
    
          cutils: Remove ioprio magic
    
           * ..and replace it with something we can call explictly where it is
             required in order to achieve the same goodness without the badness.
    
          Change-Id: I605b3acbc56ce82663dd6f1c9f3320d0ab75e178
    
      From: Zhao Wei Liew <zhaoweiliew@gmail.com>
      Date: Thu, 08 Sep 2016 20:01:29 +0800
    
          cutils: iosched_policy: Access BFQIO cgroup in /dev/bfqio
    
          AOSP doesn't mount cgroups in /sys/fs/cgroup, but in /dev/.
          Follow what AOSP does.
    
          Change-Id: I40d2241e3e5c41612d3a54d22981d3250f8b1ed6

--- libbfqio/bfqio.c
+++ libbfqio/bfqio.c
@@ -16,14 +16,11 @@
 
 #define LOG_TAG "bfqio"
 
-#include <errno.h>
 #include <fcntl.h>
 #include <cutils/iosched_policy.h>
 #include <cutils/log.h>
 #include <pthread.h>
-#include <string.h>
 #include <sys/stat.h>
-#include <unistd.h>
 
 static int __rtio_cgroup_supported = -1;
 static pthread_once_t __rtio_init_once = PTHREAD_ONCE_INIT;
