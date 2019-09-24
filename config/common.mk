47baec8a49fe1cb7214f2bdd28a49aadc08e08dc
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 20:18:11 +0200

    prebuilt: Add a script to simplify A-only recovery system mount
    * This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q
    
    Change-Id: Ib86640f5d2d6a5e518576fda895cd04e0fd35112

--- config/common.mk
+++ config/common.mk
@@ -40,7 +40,11 @@ PRODUCT_COPY_FILES += \
     vendor/xtended/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
 endif
 
-# Backup Services whitelist
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/xtended/build/tools/system-mount.sh:install/bin/system-mount.sh
+
+# Backup services whitelist
 PRODUCT_COPY_FILES += \
     vendor/xtended/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
 
