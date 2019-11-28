138dfaaf65121739227b76cb25e204f12372f64b
SuperDroidBond <superdroidbond@yahoo.com>
2019-11-28 22:18:15 +0530

    add back "ro.build.selinux" System Properties
    **This is used to show selinux status in Settings
    
    This reverts commit c0246081e5d5672f762e109f6198dee9a445df4f.

--- config/common.mk
+++ config/common.mk
@@ -43,6 +43,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.nohdcp=1 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
+    ro.build.selinux=1 \
     persist.sys.dun.override=0 \
     ro.com.google.ime.theme_id=5 \
     persist.debug.wfd.enable=1 \
