1db37de1b55cc3512974a641dd1ef662d9fde3e3
Anish Pratheepan <42488259+ultranoob-5@users.noreply.github.com>
2020-03-02 15:11:29 +0530

    fix derp in 8d2fa1

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -53,7 +53,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka xtended
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -475,7 +475,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                xtended|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
