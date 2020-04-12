7e7b711a10b64b8ac89424b50fdf8e2f10a5c801
Lup Gabriel <gwolf2u.website@gmail.com>
2020-04-12 12:39:36 +0530

    vendor: fix changelog generation after repo tool update
    Thanks to @alex9yust for further changes

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -15,7 +15,7 @@ for i in $(seq 14);
 do
 export After_Date=`date --date="$i days ago" +%Y/%m/%d`
 k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
+      export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
 
 	# Line with after --- until was too long for a small ListView
 	echo '=======================' >> $Changelog;
@@ -23,9 +23,16 @@ k=$(expr $i - 1)
 	echo '=======================' >> $Changelog;
 	echo >> $Changelog;
 
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
-	echo >> $Changelog;
+# Cycle through every repo to find commits between 2 dates
+while read path;
+        do
+    CLOG=$(git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date);
+    if [ -n "$CLOG" ]; then
+       echo project $path >> $Changelog
+       git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date >> $Changelog
+    fi
+done < ./.repo/project.list;
+echo "" >> $Changelog;
 done
 
 sed -i 's/project/   */g' $Changelog
