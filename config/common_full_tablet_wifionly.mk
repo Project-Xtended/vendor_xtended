ed5079dd534e7c2116847c4e27957e0f906059aa
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-13 23:38:23 +0530

    Stop building LatinIME, Libs & Dictionary [2/2]
    Change-Id: I87c6e9e76ba38008407f5b08cdd76ff11744dabc

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,9 +1,2 @@
 # Inherit full common Xtended stuff
 $(call inherit-product, vendor/xtended/config/common_full.mk)
-
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
-# Include Xtended LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/xtended/overlay/dictionaries
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,8 +1,4 @@
 # Inherit mini common Xtended stuff
 $(call inherit-product, vendor/xtended/config/common_mini.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 $(call inherit-product, vendor/xtended/config/telephony.mk)
