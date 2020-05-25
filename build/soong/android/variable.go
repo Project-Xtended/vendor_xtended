ffdd9e795e2f06d592efad38f789045b72a2bd1f
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:20:26 +0800

    Sort soong variables alphabetically
    Change-Id: I880b07972fefb7fa2b40ab4aeacd073fb9a942b4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,7 +3,13 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
-        Device_support_hwfde struct {
+	Apply_msm8974_1440p_egl_workaround struct {
+		Cflags []string
+	}
+	Bootloader_message_offset struct {
+		Cflags []string
+	}
+	Device_support_hwfde struct {
 		Cflags []string
 		Header_libs []string
 		Shared_libs []string
@@ -11,13 +17,7 @@ type Product_variables struct {
 	Device_support_hwfde_perf struct {
 		Cflags []string
 	}
-        Device_support_legacy_hwfde struct {
-		Cflags []string
-	}
-	Apply_msm8974_1440p_egl_workaround struct {
-		Cflags []string
-	}
-	Bootloader_message_offset struct {
+	Device_support_legacy_hwfde struct {
 		Cflags []string
 	}
 	Has_legacy_camera_hal1 struct {
@@ -42,19 +42,22 @@ type Product_variables struct {
 	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
+	Target_camera_needs_client_info struct {
+		Cflags []string
+	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
+		Cppflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
+	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
 	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
 	Target_shim_libs struct {
-		Cppflags []string
-	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
-	}
-	Target_ignores_ftp_pptp_conntrack_failure struct {
 		Cppflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
@@ -69,9 +72,6 @@ type Product_variables struct {
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
-	}
-	Target_camera_needs_client_info struct {
-                Cflags []string
 	}
 	Uses_qcom_um_family struct {
 		Cflags []string
@@ -104,11 +104,11 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
+	Bootloader_message_offset  *int `json:",omitempty"`
 	Device_support_hwfde  *bool `json:",omitempty"`
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
 	Device_support_legacy_hwfde  *bool `json:",omitempty"`
-	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
-	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
@@ -116,16 +116,16 @@ type ProductVariables struct {
 	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Target_camera_needs_client_info  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
-	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
-	Target_camera_needs_client_info  *bool `json:",omitempty"`
 	Uses_qcom_um_family  *bool `json:",omitempty"`
 	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
