bdf9dbf7c59c6868f50eaec7d2feab2af282782a
xawlw <abdulazizawlw@gmail.com>
2020-07-31 12:24:47 +0300

    repopick: Update URL
    Change-Id: I8f2b6f2dbb56902af581ca5cb8b87bbf3ccae891

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -154,7 +154,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to MSM-Xtended Gerrit
-    default_gerrit = 'https://review.msmxtended.me'
+    default_gerrit = 'https://review.msmxtended.org'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
