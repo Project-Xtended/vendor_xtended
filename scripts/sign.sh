1e61d08c096c7a299d4d2b3c7e04be4fd09894de
Dev <devvmashru@protonmail.ch>
2021-05-20 00:17:04 +0530

    vendor: sign: Display file location after compilation is successful

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 XTENDED_VERSION=`get_build_var XTENDED_VERSION`
+FILE_NAME="$XTENDED_VERSION.zip"
 echo "Signing Files"
 sign_target_files_apks -o --default_key_mappings .android-certs $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $OUT/signed-target_files.zip
 echo "Generating OTA from target files"
-ota_from_target_files -k  .android-certs/releasekey $OUT/signed-target_files.zip $OUT/"$XTENDED_VERSION.zip"
+ota_from_target_files -k  .android-certs/releasekey $OUT/signed-target_files.zip $OUT/$FILE_NAME
+echo "Package completed successfully"
+echo "Zip: $OUT/$FILE_NAME"
