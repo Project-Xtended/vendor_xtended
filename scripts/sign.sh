bee927c3dfc4925a8dc14cced9227f759eabb6a9
Dev <devvmashru@protonmail.ch>
2021-05-24 00:40:07 +0530

    vendor: scripts: Enable backuptool
    Signed-off-by: Dev <devvmashru@protonmail.ch>

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -5,7 +5,7 @@ FILE_NAME="$XTENDED_VERSION.zip"
 echo "Signing Files"
 if sign_target_files_apks -o --default_key_mappings .android-certs $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $OUT/signed-target_files.zip; then
 	echo "Generating OTA from target files"
-	if ota_from_target_files -k .android-certs/releasekey $OUT/signed-target_files.zip $OUT/$FILE_NAME; then
+	if ota_from_target_files -k .android-certs/releasekey --backup=true $OUT/signed-target_files.zip $OUT/$FILE_NAME; then
 		echo "Package completed successfully"
 		echo "Zip: $OUT/$FILE_NAME"
 	fi
