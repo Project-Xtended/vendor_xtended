fa8c0eda8fb07fc67e11d0a4d3e8a3696cb3cc1b
kubersharma001 <kubersharma001@gmail.com>
2021-10-26 13:38:06 +0000

    vendor: props: Disable blur on App Launch
    Ref: https://android.googlesource.com/platform/packages/apps/Launcher3/+/refs/tags/android-12.0.0_r3/quickstep/src/com/android/launcher3/statehandlers/DepthController.java#232
    
    Change-Id: Iab609dc9be5fbb8ccc5e124420a214e261fdc931

--- config/common.mk
+++ config/common.mk
@@ -79,6 +79,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Blur
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=false
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
