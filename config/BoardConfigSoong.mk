12b2eb4c2899a6ea5f8c6b8e35a011a188bed626
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-02-10 14:23:50 +0530

    xtended:soong: Add camera_needs_client_info_lib
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,6 +37,7 @@ SOONG_CONFIG_xtendedGlobalVars += \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     camera_needs_client_info \
+    camera_needs_client_info_lib \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
@@ -74,6 +75,7 @@ SOONG_CONFIG_xtendedGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IG
 SOONG_CONFIG_xtendedGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_xtendedGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_xtendedGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_xtendedGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_xtendedNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_xtendedQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_xtendedQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
