42cb5d8e940f421ac0911bc753dc262fb1206c69
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:39:47 +0300

    config: Remove TARGET_USES_QCOM_BSP_LEGACY
    Change-Id: I93f111285c46e1a1f811b9d8555de96474d60386

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -51,9 +51,8 @@ SOONG_CONFIG_xtendedNvidiaVars += \
 SOONG_CONFIG_NAMESPACES += xtendedQcomVars
 SOONG_CONFIG_xtendedQcomVars += \
     supports_extended_compress_format \
-    uses_pre_uplink_features_netmgrd \
-    uses_qcom_bsp_legacy
-
+    uses_pre_uplink_features_netmgrd
+    
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_xtendedQcomVars += \
@@ -72,7 +71,6 @@ SOONG_CONFIG_xtendedGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPL
 SOONG_CONFIG_xtendedNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_xtendedQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_xtendedQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_xtendedQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)ray := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
