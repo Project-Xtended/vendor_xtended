854a572bbf066c5262ad88a8ed85e67a3405efa7
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-17 04:47:54 +0530

    Hookup GRALLOC_HANDLE_HAS_RESERVED_SIZE
    * Needed for camera on redbull 13
    
    Change-Id: Ibc1d941f8a6e5e61e3a56cb0065e387022fa3441

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,6 +29,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += xtendedGlobalVars
 SOONG_CONFIG_xtendedGlobalVars += \
     additional_gralloc_10_usage_bits \
+    gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib
@@ -49,12 +50,14 @@ SOONG_CONFIG_xtendedQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_xtendedGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_xtendedNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_xtendedQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_xtendedQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
+TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
