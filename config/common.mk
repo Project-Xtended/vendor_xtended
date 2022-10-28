0efa1b6060935e4869893ea351d46353ed7f6166
mukesh22584 <mks22584@gmail.com>
2022-10-28 19:48:28 +0530

    vendor: Build GameSpace

--- config/common.mk
+++ config/common.mk
@@ -119,6 +119,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_PACKAGES += \
     Aperture \
     Gallery2 \
+    GameSpace \
     XtendedThemesStub \
     ThemePicker \
     RepainterServicePriv
